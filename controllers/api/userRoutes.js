fad9f9e0c64c0bd34abcf634ef985688e024496b
Malcolm Mason <79657911+malmason@users.noreply.github.com>
2021-07-07 22:48:02 -0400

    fixed issue with new user being added

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,12 +2,15 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
+  console.log(req);
   try {
     const userData = await User.create(req.body);
 
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+      req.session.first_name = userData.first_name;
+      req.session.last_name = userData.last_name;
 
       res.status(200).json(userData);
     });
